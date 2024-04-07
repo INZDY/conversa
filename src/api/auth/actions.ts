f642757df9a0c6e1d0e6efd66b494fcf9e32d881
INZDY <57595839+INZDY@users.noreply.github.com>
2024-04-07 11:33:11 +0700

    Move and organize components.

--- src/api/auth/actions.ts
+++ src/api/auth/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import createSupabaseServerClient from "@/api/supabase/server";
+import { redirect } from "next/navigation";
 
 import { FieldValues } from "react-hook-form";
 
@@ -30,4 +31,10 @@ export async function signup(formData: FieldValues) {
   const result = await supabase.auth.signUp(data);
 
   return JSON.stringify(result);
+}
+
+export async function logout() {
+  const supabase = await createSupabaseServerClient();
+  await supabase.auth.signOut();
+  redirect("/");
 }
