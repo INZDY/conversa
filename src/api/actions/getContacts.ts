359630feb33e9e44704c68bc33323d7b53701438
INZDY <57595839+INZDY@users.noreply.github.com>
2024-04-12 00:12:41 +0700

    getContacts - Return profile and contacts of the provided profileName. getProfiles - Return all profiles of the user.

---
+++ src/api/actions/getContacts.ts
@@ -0,0 +1,30 @@
+import prisma from "@/server/prisma";
+import getSession from "./getSession";
+
+export default async function getContacts(profileName: string) {
+  const sessionData = await getSession();
+
+  if (!sessionData) {
+    return [];
+  }
+
+  try {
+    const userContacts = await prisma.profile.findFirst({
+      where: {
+        userId: sessionData.id as string,
+        name: profileName,
+      },
+      include: {
+        contacts: true,
+      },
+    });
+
+    if (!userContacts) {
+      return [];
+    }
+
+    return userContacts.contacts;
+  } catch (error: any) {
+    return [];
+  }
+}
--- src/api/actions/getProfiles.ts
+++ src/api/actions/getProfiles.ts
@@ -2,25 +2,21 @@ import prisma from "@/server/prisma";
 import getSession from "./getSession";
 
 export default async function getProfiles() {
-  try {
-    const sessionData = await getSession();
+  const sessionData = await getSession();
 
-    if (!sessionData) {
-      return null;
-    }
-
-    const userProfiles = await prisma.profile.findMany({
-      where: {
-        userId: sessionData.id as string,
-      },
-    });
-
-    if (!userProfiles) {
-      return null;
-    }
-
-    return userProfiles;
-  } catch (error: any) {
+  if (!sessionData) {
     return null;
   }
+
+  const userProfiles = await prisma.profile.findMany({
+    where: {
+      userId: sessionData.id as string,
+    },
+  });
+
+  if (!userProfiles) {
+    return null;
+  }
+
+  return userProfiles;
 }
