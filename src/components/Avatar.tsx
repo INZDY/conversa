acc289365e633e4f6f5d818122e8df2f72e03d1e
INZDY <57595839+INZDY@users.noreply.github.com>
2024-05-02 21:09:53 +0700

    Some fixes from the merged branch.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,39 +1,29 @@
-"use client"
+"use client";
 
 import { Profile } from "@prisma/client";
-
 import Image from "next/image";
 
-
-interface AvatarProps{
-    profiles?: Profile; 
+interface AvatarProps {
+  profile?: Profile;
 }
 
-const Avatar: React.FC<AvatarProps> = ({
-    profiles
-}) =>{
-    return(
-        <div className="relative">
-           <div
-                className="
-                relative
-                inline-block
-                rounded-full
-                over-flow-hidden
-                h-9
-                w-9
-                md:h-11
-                md:w-11
-                "
-           >
-            <Image
-                alt="Avatar"
-                src={profiles ?.image|| '/placeholder.png'}
-                    fill
-                />
-           </div>
-        </div>
-    );
+export default function Avatar({ profile }: AvatarProps) {
+  return (
+    <div className="relative">
+      <div
+        className="
+            relative
+            inline-block
+            rounded-full
+            overflow-hidden
+            h-9
+            w-9
+            md:h-11
+            md:w-11
+            "
+      >
+        <Image alt="Avatar" src={profile?.image || "/placeholder.png"} fill />
+      </div>
+    </div>
+  );
 }
-
-export default Avatar;
