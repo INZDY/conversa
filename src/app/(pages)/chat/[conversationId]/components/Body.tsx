38d5089dc2a10dfb82646869b5d9fcbe9df31e38
INZDY <57595839+INZDY@users.noreply.github.com>
2024-05-05 16:23:09 +0700

    Pusher for MessageBox body and message route. Install lodash package.

--- src/app/(pages)/chat/[conversationId]/components/Body.tsx
+++ src/app/(pages)/chat/[conversationId]/components/Body.tsx
@@ -5,6 +5,8 @@ import { FullMessageType } from "@/types";
 import { useEffect, useRef, useState } from "react";
 import MessageBox from "./MessageBox";
 import axios from "axios";
+import { pusherClient } from "@/server/pusher";
+import { find } from "lodash";
 
 interface BodyProps {
   initialMessages: FullMessageType[];
@@ -19,6 +21,32 @@ export default function Body({ initialMessages }: BodyProps) {
   useEffect(() => {
     axios.post(`/api/conversations/${conversationId}/seen`);
   }, [conversationId]);
+
+  useEffect(() => {
+    pusherClient.subscribe(String(conversationId));
+    bottomRef?.current?.scrollIntoView();
+
+    const messageHandler = (message: FullMessageType) => {
+      axios.post(`/api/conversations/${conversationId}/seen`);
+
+      setMessages((current) => {
+        if (find(current, { id: message.id })) {
+          return current;
+        }
+
+        return [...current, message];
+      });
+
+      bottomRef?.current?.scrollIntoView();
+    };
+
+    pusherClient.bind("messages:new", messageHandler);
+
+    return () => {
+      pusherClient.unsubscribe(String(conversationId));
+      pusherClient.unbind("messages:new", messageHandler);
+    };
+  }, [[conversationId]]);
 
   return (
     <div className="flex-1 overflow-y-auto">
